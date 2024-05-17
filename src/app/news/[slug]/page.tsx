ed4482f0598f3dfa26c73c91e83a284348e98744
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2024-05-17 11:18:00 -0300

    feat: showing dummy news details

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,9 +1,26 @@
+import DUMMY_NEWS from "@/lib/dummy-news";
+import { notFound } from "next/navigation";
+
 interface Params {
   readonly params: { slug: string };
 }
 
 export default function NewsDetailPage({ params }: Params) {
   const slug = params.slug;
+  const news = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
 
-  return <h1>{slug}</h1>;
+  if (!news) {
+    notFound();
+  }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`images/news/${news.image}`} alt={news.title} />
+        <h1>{news.title}</h1>
+        <time dateTime={news.date}>{news.date}</time>
+      </header>
+      <p>{news.content}</p>
+    </article>
+  );
 }
